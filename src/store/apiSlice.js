90546ef9f1f55bd0f7d30cd8bdd237d87596a402
hasanhabib123 <freelancerhasan401@gmail.com>
2023-01-03 11:33:04 +0600

    add api uri

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseURI = 'http://localhost:8080';
+//const baseURI = 'http://localhost:8080';
+const baseURI ='https://mern-expenses-tracker-backend.onrender.com';
 
 export const apiSlice = createApi({
     baseQuery : fetchBaseQuery({ baseUrl : baseURI}),
